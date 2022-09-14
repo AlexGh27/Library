23678bcdeef0f133c3cae829d985c72a9900bf62
AlexGh27 <alexghioca27@gmail.com>
2022-09-14 22:13:38 +0300

    Built the addBook function

--- script.js
+++ script.js
@@ -15,5 +15,9 @@ function addBookToLibrary() {
     let bookTitle = document.querySelector("#book_title").value;
     let bookAuthor = document.querySelector("#book_author").value;
     
-    
+    let book = new Book(bookTitle, bookAuthor);
+    myLibrary.push(book);
+    console.log(myLibrary);
+    document.querySelector("#book_title").value = "";
+    document.querySelector("#book_author").value = "";
 }

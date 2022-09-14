ddcd0e402c59bd9d7a422aa8e158fddda9c4cc1c
AlexGh27 <alexghioca27@gmail.com>
2022-09-14 10:51:38 +0300

    Function definition

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+const addBook = document.querySelector('#addBook')
+
+
+addBook.addEventListener('click', function(e) {
+    addBookToLibrary();
+})
+
 let myLibrary = [];
 
-function Book(title, author, year) {
+function Book(title, author) {
     this.title = title;
-    this.author = author;
-    this.year = year;
+    this.author = author;  
 }
 
 function addBookToLibrary() {
-
+    let title = document.getElementById("#book_title").value;
+    let author = document.getElementById("#book_author").value;
+    
+    console.log(author);
 }
